87df820cfce8a57a26fb53cbc49f51c0966bcf59
Pipaolo <mastastah8975@gmail.com>
2018-10-07 22:41:29 +0800

    Added RentalServiceProj

---
+++ 1st_Term_Project_(Rental Service)/Sales.h
@@ -0,0 +1,17 @@
+#ifndef _SALES_
+#define _SALES_
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <fstream>
+using std::cin;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+
+
+
+#endif
