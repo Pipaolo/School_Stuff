a11ad24061afcce2062424c34ed7044e8daa0bf6
Pipaolo <mastastah8975@gmail.com>
2018-10-09 18:00:28 +0800

    Add main.h (Contains all the needed libraries)

---
+++ 1st_Term_Project_-Rental Service/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_
+#define _MAIN_
+#include <iostream>
+#include <vector>
+#include <string>
+#include <windows.h>
+#include <fstream>
+
+using std::cin;
+using std::string;
+using std::cout;
+using std::endl;
+
+#endif
