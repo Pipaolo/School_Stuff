7e4214670a8daf2ce06a9db12c22eb6cbb8db58b
Pipaolo <mastastah8975@gmail.com>
2019-02-06 14:56:33 +0800

    Second Push

---
+++ C#_Programming/3rd_Act/2nd_App/2nd_App/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _2nd_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Modify exercise number 1 and allow positive and negative numbers as inputs to be multiplied.  
+         * Hint:  Take absolute values first then use condition statements. 
+         * 
+         * 
+         * 
+         */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int firstInput = 0;
+            int secondInput = 0;
+            int result = 0;
+
+            string first_Input = Interaction.InputBox("Enter 1st Number", "2nd_App", "---");
+            string second_Input = Interaction.InputBox("Enter 2nd Number", "2nd_App", "---");
+
+            firstInput = Convert.ToInt32(first_Input);
+            secondInput = Convert.ToInt32(second_Input);
+            
+            if (firstInput < 0 && secondInput >= 0 || firstInput >= 0 && secondInput < 0)
+            {
+                for (int i = 0; i < secondInput; i++)
+                {
+                    result += firstInput;
+                }
+            }
+            else
+            {
+                firstInput *= -1;
+                secondInput *= -1;
+                for (int i = 0; i < secondInput; i++)
+                {
+                    result += firstInput;
+                }
+            }
+
+            MessageBox.Show($"Output is: {result}");
+            this.Close();
+        }
+    }
+}
---
+++ C#_Programming/3rd_Act/3rd_App/3rd_App/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _3rd_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Write a program that will compute the quotient (including the remainder) of two positive integers (firstno divided by secondno) by subtracting them repeatedly.  
+         * This means subtracting firstno to secondno repeatedly until the difference is less than secondno.
+         * 
+         * 
+         */
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int firstInput = 0;
+            int secondInput = 0;
+            int quotient = 0;
+
+
+            string first_Input = Interaction.InputBox("Enter 1st Number", "3rd_App", "---");
+            string second_Input = Interaction.InputBox("Enter 2nd Number", "3rd_App", "---");
+
+            firstInput = Convert.ToInt32(first_Input);
+            secondInput = Convert.ToInt32(second_Input);
+
+            while (firstInput != 0)
+            {
+                firstInput -= secondInput;
+                quotient++;
+            }
+            
+            MessageBox.Show($"Quotient is: {quotient}, Remainder: {firstInput}");
+            this.Close();
+        }
+    }
+}
