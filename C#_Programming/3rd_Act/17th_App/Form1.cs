9233598c2fdb188e0be2085d7d45aa25a714fe83
Pipaolo <mastastah8975@gmail.com>
2019-02-06 20:53:07 +0800

    Finished all up to 20th_App

---
+++ C#_Programming/3rd_Act/17th_App/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _17th_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string finalOutput = "";
+            string user_Input = Interaction.InputBox("Enter Number", "16th_App", "---");
+            user_Input = user_Input.Replace(" ", "");
+
+            foreach (char index in user_Input)
+            {
+                finalOutput += index + "\n";
+            }
+            MessageBox.Show(finalOutput);
+            this.Close();
+        }
+    }
+}
---
+++ C#_Programming/3rd_Act/20th_App/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _20th_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string finalOutput = "";
+            string user_Input = Interaction.InputBox("Enter Number", "20th_App", "---");
+            string inputCopy = user_Input;
+            int charCount = 0;
+            int counter = 0;
+            int[] inputCount = new int[user_Input.Length];
+            foreach (char index in user_Input)
+            {               
+                charCount = 0;
+                foreach (char index2 in inputCopy)
+                {
+                    if (index == index2)
+                    {
+                        charCount++;
+                    }
+                }
+                inputCount[counter] = charCount;
+                counter++;
+            }
+
+            counter = 0;
+            foreach (char index in user_Input)
+            {
+
+                finalOutput += index.ToString() + "\t\t" + inputCount[counter].ToString() + "\n";
+                counter++;
+            }
+            finalOutput = finalOutput.Replace(" ", "space");
+            MessageBox.Show(finalOutput);
+            this.Close();
+        }
+    }
+}
