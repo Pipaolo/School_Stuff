3ff91c855024bed89e7ecf454f0f3f2f1082d7d9
Pipaolo <mastastah8975@gmail.com>
2019-02-06 14:26:20 +0800

    Start input 3rd Activity

--- C#_Programming/2nd_Act/4th_App/4th_App/Program.cs
+++ C#_Programming/2nd_Act/4th_App/4th_App/Program.cs
@@ -42,112 +42,116 @@ namespace _4th_App
 
             userInput = Convert.ToInt32(Console.ReadLine());
             getOnes:
-            if (userInput < 10)
+            while(userInput != 0)
             {
-                if (userInput < 4)
+                if (userInput < 10)
                 {
-                    for (int i = 0; i < userInput; i++)
+                    if (userInput < 4)
                     {
-                        Console.Write(one);
-                    }
-                }
-                else
-                {
-                    if (userInput == 4)
-                        Console.Write(four);
-                    else if (userInput == 5)
-                        Console.Write(five);
-                    else if (userInput > 5 && userInput < 9)
-                    {
-                        for (int i = 0; i < userInput - 4; i++)
+                        for (int i = 0; i < userInput; i++)
                         {
-                            if (i == 0)
-                                Console.Write(five);
-                            else
-                                Console.Write(one);
+                            Console.Write(one);
                         }
                     }
                     else
-                        Console.Write(nine);
-                }
-            }
-            else if (userInput >= 10 && userInput < 100)
-            {
-                if (userInput < 40)
-                {
-                    for (int i = 0; i < userInput / 10; i++)
                     {
-                        Console.Write(ten);
+                        if (userInput == 4)
+                            Console.Write(four);
+                        else if (userInput == 5)
+                            Console.Write(five);
+                        else if (userInput > 5 && userInput < 9)
+                        {
+                            for (int i = 0; i < userInput - 4; i++)
+                            {
+                                if (i == 0)
+                                    Console.Write(five);
+                                else
+                                    Console.Write(one);
+                            }
+                        }
+                        else
+                            Console.Write(nine);
+                        userInput -= userInput;
                     }
                 }
-                else
+                else if (userInput >= 10 && userInput < 100)
                 {
-                    if (userInput == 40)
-                        Console.Write(forty);
-                    else if (userInput == 50)
-                        Console.Write(fifty);
-                    else if (userInput > 50 && userInput < 90)
+                    if (userInput < 40)
                     {
-                        for (int i = 0; i < (userInput - 40) / 10; i++)
+                        for (int i = 0; i < userInput / 10; i++)
                         {
-                            if (i == 0)
-                                Console.Write(fifty);
-                            else
-                                Console.Write(ten);
+                            Console.Write(ten);
                         }
                     }
                     else
-                        Console.Write(ninety);
-                }
-                userInput %= 10;
-                goto getOnes;
-            }
-            else if (userInput >= 100 && userInput < 1000)
-            {
-                if (userInput < 400)
-                {
-                    for (int i = 0; i < userInput / 100; i++)
                     {
-                        Console.Write(oneHundred);
-                    }
-                }
-                else
-                {
-                    if (userInput == 400)
-                        Console.Write(fourHundred);
-                    else if (userInput == 500)
-                        Console.Write(fiveHundred);
-                    else if (userInput > 500 && userInput < 900)
-                    {
-                        for (int i = 0; i < userInput - 400; i++)
+                        if (userInput == 40)
+                            Console.Write(forty);
+                        else if (userInput == 50)
+                            Console.Write(fifty);
+                        else if (userInput > 50 && userInput < 90)
                         {
-                            if (i == 0)
-                                Console.Write(fiveHundred);
-                            else
-                                Console.Write(oneHundred);
+                            for (int i = 0; i < (userInput - 40) / 10; i++)
+                            {
+                                if (i == 0)
+                                    Console.Write(fifty);
+                                else
+                                    Console.Write(ten);
+                            }
+                        }
+                        else
+                            Console.Write(ninety);
+                    }
+                    userInput %= 10;
+                    goto getOnes;
+                }
+                else if (userInput >= 100 && userInput < 1000)
+                {
+                    if (userInput < 400)
+                    {
+                        for (int i = 0; i < userInput / 100; i++)
+                        {
+                            Console.Write(oneHundred);
                         }
                     }
                     else
-                        Console.Write(nineHundred);
+                    {
+                        if (userInput == 400)
+                            Console.Write(fourHundred);
+                        else if (userInput == 500)
+                            Console.Write(fiveHundred);
+                        else if (userInput > 500 && userInput < 900)
+                        {
+                            for (int i = 0; i < userInput - 400; i++)
+                            {
+                                if (i == 0)
+                                    Console.Write(fiveHundred);
+                                else
+                                    Console.Write(oneHundred);
+                            }
+                        }
+                        else
+                            Console.Write(nineHundred);
+                    }
+                    userInput %= 100;
+                    goto getOnes;
                 }
-                userInput %= 100;
-                goto getOnes;
-            }
-            else if (userInput >= 1000 && userInput < 4000)
-            {
+                else if (userInput >= 1000 && userInput < 4000)
+                {
 
-                for (int i = 0; i < userInput / 1000; i++)
+                    for (int i = 0; i < userInput / 1000; i++)
+                    {
+                        Console.Write(oneThousand);
+                    }
+                    userInput %= 1000;
+                    goto getOnes;
+                }
+                else
                 {
-                    Console.Write(oneThousand);
-                }           
-                userInput %= 1000;
-                goto getOnes;
-            }
-            else
-            {
-                Console.WriteLine("You cannot enter a value above 3999!");
-                Console.Read();
-            }
+                    Console.WriteLine("You cannot enter a value above 3999!");
+                    Console.Read();
+                }
+            }       
             Console.Read();
         }
     }
