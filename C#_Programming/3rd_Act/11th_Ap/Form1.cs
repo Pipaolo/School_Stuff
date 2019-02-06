2fcd31b99214bf6bf96c37a20f67e64c5fc40277
Pipaolo <mastastah8975@gmail.com>
2019-02-06 19:23:43 +0800

    Added activities up to number 14

---
+++ C#_Programming/3rd_Act/11th_Ap/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _11th_Ap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * Write a program that reverses the input number.  
+         * Formulate an equation to come up with the answer.  
+         * (Apply the loop statements in your solutions).  
+         * For example, an input of 1238 will produce 8321.
+         * 
+         * 
+         * 
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int userInput = 0;
+            int reversed = 0;
+            int temp = 0;
+
+            string user_Input = Interaction.InputBox("Enter Number", "11th_App", "---");
+
+            userInput = Convert.ToInt32(user_Input);
+
+            while(userInput > 0)
+            {
+                temp = userInput % 10;
+                reversed = (reversed * 10) + temp;
+                userInput /= 10;         
+            }
+
+            MessageBox.Show(reversed.ToString());
+            this.Close();
+        }
+    }
+}
---
+++ C#_Programming/3rd_Act/14th_App/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _14th_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Write a program to sum the digits in a number and print it.  
+         * For example, the sum of the digits in 347 is 14.
+         */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> listOutput = new List<int>();
+            int userInput = 0;
+            int result = 0;
+            int temp = 0;
+            string output = "";
+
+            string user_Input = Interaction.InputBox("Enter Number", "14th_App", "---");
+
+            userInput = Convert.ToInt32(user_Input);
+
+            while (userInput > 0)
+            {
+                temp = userInput % 10;
+                userInput /= 10;
+                listOutput.Add(temp);
+                result += temp;
+            }
+
+            for (int i = 1; i <= listOutput.Count; i++)
+            {
+                output += listOutput[listOutput.Count - i] + "+";
+            }
+
+            output = output.Remove(output.Length - 1);
+            MessageBox.Show(output + " = " + result);
+            this.Close();
+        }
+    }
+}
