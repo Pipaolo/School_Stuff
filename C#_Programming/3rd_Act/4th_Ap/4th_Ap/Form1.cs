d2af42dea6adb132fcbd00397cd2b5516b83ad79
Pipaolo <mastastah8975@gmail.com>
2019-02-06 15:32:55 +0800

    Add other fixes and shizz

---
+++ C#_Programming/3rd_Act/4th_Ap/4th_Ap/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _4th_Ap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Write a program that will add all prime number by letting the user specify the range of number to process.
+         * 
+         */
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bool primeChecker = true;
+            int result = 0;
+
+            string userInput = Interaction.InputBox("Enter Range", "4th_App", "---");
+
+            for (int i = 2; i <= Convert.ToInt32(userInput); i++)
+            {
+                for (int j = 2; j <= Convert.ToInt32(userInput); j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        primeChecker = false;
+                        break;
+                    }
+                }
+
+                if (primeChecker)
+                {
+                    result += Convert.ToInt32(i);
+                }
+                primeChecker = true;
+            }
+
+            MessageBox.Show($"Output is: {result}");
+            this.Close();
+
+
+        }
+    }
+}
---
+++ C#_Programming/3rd_Act/5th_App/5th_App/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace _5th_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         * Write a program that gets the Fibonacci of a number read from the user.  
+         * The Fibonacci number sequence begins with 1, 1 then each succeeding number in the sequence is the sum of the previous ones.  
+         * For example, an input of 5 will produce 1, 1, 2, 3, 5 and an input of 8 will produce 1, 1, 2, 3, 5, 8, 13, 21 and  an input of 2 will produce 1, 1.
+         */
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int userInput = 0;
+            int firstNum = 0;
+            int secondNum = 1;
+            int storeNum = 0;
+            string result = "";
+
+            string user_Input = Interaction.InputBox("Enter 1st Number", "2nd_App", "---");
+
+            userInput = Convert.ToInt32(user_Input);
+
+            int[] output = new int[userInput];
+
+            output[0] = firstNum + 1;
+
+            for(int i = 1; i < userInput; i++)
+            {
+                storeNum = firstNum + secondNum;           
+                output[i] = storeNum;
+                firstNum = secondNum;
+                secondNum = storeNum;
+            }
+        
+            foreach(int index in output)
+            {
+                result += Convert.ToString(index) + ",";
+            }
+
+            result = result.Remove(result.Length - 1);
+            MessageBox.Show($"Output is: {result}");
+            this.Close();
+        }
+    }
+}
