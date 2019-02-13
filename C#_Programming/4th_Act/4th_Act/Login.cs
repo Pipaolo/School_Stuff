0a0edbfb24c0fcba104c98142f06aae28f9bce00
Pipaolo <mastastah8975@gmail.com>
2019-02-13 16:00:27 +0800

    Added 4th Activity it's not finished yet

---
+++ C#_Programming/4th_Act/4th_Act/Login.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4th_Act
+{
+    public partial class Login : Form
+    {
+        public string username = "pipaolo";
+        public string password = "123";
+        string userInput = "";
+        string passInput = "";
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUsername_Clicked(object sender, EventArgs e)
+        {
+            txtUsername.Text = "";
+        }
+
+        private void txtPassword_Clicked(object sender, EventArgs e)
+        {
+            txtPass.Text = "";
+            txtPass.UseSystemPasswordChar = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Check_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                userInput = txtUsername.Text;
+                passInput = txtPass.Text;
+                if (username == userInput && password == passInput)
+                {
+                    MessageBox.Show("Login Success!!");
+                    Main_Menu menu = new Main_Menu();
+                    this.Hide();
+                    menu.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect Username or Password!");
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            userInput = txtUsername.Text;
+            passInput = txtPass.Text;
+            if (username == userInput && password == passInput)
+            {
+                MessageBox.Show("Login Success!!");
+                Main_Menu menu = new Main_Menu();
+                this.Hide();
+                menu.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect Username or Password!");
+            }
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            txtUsername.Text = "Username";
+            txtPass.Text = "Password";
+        }
+
+    }
+}
---
+++ C#_Programming/4th_Act/4th_Act/Main_Menu.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace _4th_Act
+{
+    partial class Main_Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Chix Burger");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Chiz Burger");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Mc Cintosh");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("One Fifth Pounder");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Burgers", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Nonald\'s Special Chicken");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Spicy Manowk");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Manowk w/ Spag");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("2pc Manowk w/ Rice");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Siomai Rice");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Meals", new System.Windows.Forms.TreeNode[] {
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode9,
+            treeNode10});
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Sinigang na water");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Sweetened Sugar");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Fried Oil");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Salted Sodium Cloride");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Apple Flavored Apple Pie");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Chicken Flavored Beef Steak");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Specials", new System.Windows.Forms.TreeNode[] {
+            treeNode12,
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16,
+            treeNode17});
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelWelcome = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.btnCheckOut = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_4th_Act.Properties.Resources.McD_Aims_to_Expand_Brand_Delivery_Business11;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(506, 221);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelWelcome
+            // 
+            this.labelWelcome.AutoSize = true;
+            this.labelWelcome.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWelcome.Location = new System.Drawing.Point(90, 168);
+            this.labelWelcome.Name = "labelWelcome";
+            this.labelWelcome.Size = new System.Drawing.Size(330, 37);
+            this.labelWelcome.TabIndex = 2;
+            this.labelWelcome.Text = "Welcome Customer!";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheckOut);
+            this.groupBox1.Controls.Add(this.treeView1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 207);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(480, 417);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(6, 14);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Chix_Burger";
+            treeNode1.Text = "Chix Burger";
+            treeNode2.Name = "Chiz_Burger";
+            treeNode2.Text = "Chiz Burger";
+            treeNode3.Name = "Mc_Cintosh";
+            treeNode3.Text = "Mc Cintosh";
+            treeNode4.Name = "One_Fifth_Pounder";
+            treeNode4.Text = "One Fifth Pounder";
+            treeNode5.Name = "Burgers";
+            treeNode5.Text = "Burgers";
+            treeNode6.Name = "Nonald_Special";
+            treeNode6.Text = "Nonald\'s Special Chicken";
+            treeNode7.Name = "Spicy_Manowk";
+            treeNode7.Text = "Spicy Manowk";
+            treeNode8.Name = "Manowk_Spag";
+            treeNode8.Text = "Manowk w/ Spag";
+            treeNode9.Name = "2_Manowk";
+            treeNode9.Text = "2pc Manowk w/ Rice";
+            treeNode10.Name = "Siomai_Rice";
+            treeNode10.Text = "Siomai Rice";
+            treeNode11.Name = "Meal";
+            treeNode11.Text = "Meals";
+            treeNode12.Name = "Sinigang";
+            treeNode12.Text = "Sinigang na water";
+            treeNode13.Name = "Sugar";
+            treeNode13.Text = "Sweetened Sugar";
+            treeNode14.Name = "Oil";
+            treeNode14.Text = "Fried Oil";
+            treeNode15.Name = "Salt";
+            treeNode15.Text = "Salted Sodium Cloride";
+            treeNode16.Name = "ApplePie";
+            treeNode16.Text = "Apple Flavored Apple Pie";
+            treeNode17.Name = "Chick_Beef";
+            treeNode17.Text = "Chicken Flavored Beef Steak";
+            treeNode18.Name = "Special";
+            treeNode18.Text = "Specials";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode11,
+            treeNode18});
+            this.treeView1.Size = new System.Drawing.Size(255, 397);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect_1);
+            // 
+            // btnCheckOut
+            // 
+            this.btnCheckOut.Location = new System.Drawing.Point(267, 326);
+            this.btnCheckOut.Name = "btnCheckOut";
+            this.btnCheckOut.Size = new System.Drawing.Size(205, 85);
+            this.btnCheckOut.TabIndex = 2;
+            this.btnCheckOut.Text = "Check Out";
+            this.btnCheckOut.UseVisualStyleBackColor = true;
+            // 
+            // Main_Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(490, 627);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.labelWelcome);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Main_Menu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main_Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelWelcome;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button btnCheckOut;
+    }
+}
